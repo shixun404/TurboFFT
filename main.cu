ece876c028864b57a0074bd68ed10fba547c9831
shixun404 <shixun404@gmail.com>
2024-01-11 22:37:26 -0800

    update

--- main.cu
+++ main.cu
@@ -1,7 +1,9 @@
 #include "include/turbofft.h"
+// #include "include/code_gen/generated/fft_radix_2_logN_8_upload_0.cuh"
+#include "include/code_gen/generated/fft_radix_2_logN_2_upload_0.cuh"
 // #define DataType nv_bfloat162
-// #define DataType double2
-#define DataType float2
+#define DataType double2
+// #define DataType float2
 // #define DataType half2
 
 
@@ -9,38 +11,51 @@
 void test_turbofft(DataType* input_d, DataType* output_d, DataType* output_turbofft, 
                     long long int N){
     dim3 gridDim(1, 1, 1); 
-    dim3 blockDim(1, 32, 1);
+    dim3 blockDim(1, 1, 1);
     // turbofft::fft::thread::fft<DataType, turbofft::Tensor<DataType, 1, 2>><<<gridDim, blockDim>>>(input_d, output_d);
     // turbofft::fft::thread::fft<DataType><<<gridDim, blockDim>>>(input_d, output_d);
-    long long int shared_size = (N * sizeof(DataType) / 16) * 17;
+    long long int shared_size = N * sizeof(DataType);
 
-    cudaFuncSetAttribute(turbofft::fft::thread::fft<DataType, 8>, cudaFuncAttributeMaxDynamicSharedMemorySize, shared_size);
-    turbofft::fft::thread::fft<DataType, 8><<<gridDim, blockDim, shared_size>>>(input_d, output_d);
+    // cudaFuncSetAttribute(turbofft::fft::thread::fft<DataType, 8>, cudaFuncAttributeMaxDynamicSharedMemorySize, shared_size);
+    cudaFuncSetAttribute(fft_radix_2_logN_2_dim_0, cudaFuncAttributeMaxDynamicSharedMemorySize, shared_size);
+    
+    
+    // turbofft::fft::thread::fft<DataType, 8><<<gridDim, blockDim, shared_size>>>(input_d, output_d);
     // turbofft::fft::thread::fft<DataType><<<gridDim, blockDim>>>(input_d, output_d);
+    // fft_radix_2_logN_8_dim_0<<<gridDim, blockDim>>>(input_d, output_d);
+    fft_radix_2_logN_2_dim_0<<<gridDim, blockDim>>>(input_d, output_d);
     cudaDeviceSynchronize();
     printf("%d\n",  N * sizeof(DataType));
     checkCudaErrors(cudaMemcpy((void*)output_turbofft, (void*)output_d, N * sizeof(DataType), cudaMemcpyDeviceToHost));
-
 }
 
 
-int main(){
+int main(int argc, char *argv[]){
     DataType* input, *output_turbofft, *output_cufft;
     DataType* input_d, *output_d;
-    long long int N = 2 << 10, bs = 2 << 10;
-    int ntest = 10;
+    long long int N = 1 << 2, bs = 1;
+    int ntest = 1;
 
-    utils::initializeData<DataType>(input, input_d, output_d, output_turbofft, output_cufft, N, bs);
-    // test_turbofft(input_d, output_d, output_turbofft, N);
+    if (argc < 2) bs = 1;
+    else bs = std::atoi(argv[1]);
+    printf("N=%d, bs=%d\n", N, bs);
+    utils::initializeData<DataType>(input, input_d, output_d, output_turbofft, output_cufft, N, bs + 3);
+    test_turbofft(input_d, output_d, output_turbofft, N);
+
+    
+    // profiler::cufft::test_cufft_ft<DataType>(input_d, output_d, output_cufft, input_d + N * (bs + 2),
+    //                                          input_d + N * (bs + 1), output_d + N * (bs + 2),   N, bs, ntest);
 
     profiler::cufft::test_cufft<DataType>(input_d, output_d, output_cufft, N, bs, ntest);
 
-    profiler::cufft::test_cufft_ft<DataType>(input_d, output_d, output_cufft, input_d + N * (bs + 2),
-                                             input_d + N * (bs + 1), output_d + N * (bs + 2),   N, bs + 1, ntest);
+    // profiler::cufft::test_cufft_ft<DataType>(input_d, output_d, output_cufft, input_d + N * (bs + 2),
+    //                                          input_d + N * (bs + 1), output_d + N * (bs + 2),   N, bs, ntest);
 
-    // utils::compareData<DataType>(output_turbofft, output_cufft, N, 1e-5);
+    
+    utils::compareData<DataType>(output_turbofft, output_cufft, N, 1e-5);
     // printData(output_turbofft, N);
     // printData(output_cufft, N);
-
+    cudaFree(input_d);
+    cudaFree(output_d);
     return 0;
 }
