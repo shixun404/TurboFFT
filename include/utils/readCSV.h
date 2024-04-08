c5c21ad3d14e67d0dad03fa7f4637fd07f0299f4
shixun404 <shixun404@gmail.com>
2024-04-07 20:21:08 -0700

    update

--- include/utils/readCSV.h
+++ include/utils/readCSV.h
@@ -5,7 +5,7 @@ std::vector<long long int> split(const std::string& s, char delimiter) {
     std::string token;
     std::istringstream tokenStream(s);
     while (std::getline(tokenStream, token, delimiter)) {
-        tokens.push_back(std::stoi(token.c_str()));
+        tokens.push_back(std::atoi(token.c_str()));
     }
     return tokens;
 }
@@ -14,7 +14,7 @@ std::vector<std::vector<long long int> > load_parameters(
     std::string &file_name, bool if_print=false){
     // std::ifstream &file, bool if_print=false){
     std::vector<std::vector<long long int> > data;
-    std::ifstream file(file_name);
+    std::ifstream file(file_name.c_str());
     
     if (file.is_open()) {
         // std::cout << "File opened successfully." << std::endl;
