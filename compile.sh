359cadb869b8c86442bc092c8959059202e31241
shixun404 <shixun404@gmail.com>
2024-04-07 22:40:20 -0700

    update

--- compile.sh
+++ compile.sh
@@ -1,10 +1,11 @@
+gpu=$1
 cd include/code_gen/scripts
-python fft_codegen.py --gpu T4 --datatype float2
-python fft_codegen.py --gpu T4 --datatype float2  --if_ft 1
-python fft_codegen.py --gpu T4 --datatype float2 --if_ft 1 --if_err_injection 1
-python fft_codegen.py --gpu T4 --datatype double2
-python fft_codegen.py --gpu T4 --datatype double2  --if_ft 1
-python fft_codegen.py --gpu T4 --datatype double2 --if_ft 1 --if_err_injection 1
+python fft_codegen.py --gpu $gpu --datatype float2
+python fft_codegen.py --gpu $gpu --datatype float2  --if_ft 1
+python fft_codegen.py --gpu $gpu --datatype float2 --if_ft 1 --if_err_injection 1
+python fft_codegen.py --gpu $gpu --datatype double2
+python fft_codegen.py --gpu $gpu --datatype double2  --if_ft 1
+python fft_codegen.py --gpu $gpu --datatype double2 --if_ft 1 --if_err_injection 1
 cd -
 cd build
 make -j
